634f470f2e6507e0910d2657fff88c15380b0c79
saravanan M <141270199+saravanan0195@users.noreply.github.com>
2025-01-24 21:15:21 +0530

    Time: 27 ms (28.33%), Space: 13.4 MB (33.21%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t): 
+            return False
+        elif sorted(s) == sorted(t): 
+            return True
+        else:
+            return False
+        
