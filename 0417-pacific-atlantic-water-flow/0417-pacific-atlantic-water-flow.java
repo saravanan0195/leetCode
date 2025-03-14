1b732c16a04bce26165fb9e0ec84b4f0b2df7763
saravanan M <141270199+saravanan0195@users.noreply.github.com>
2025-03-14 22:49:00 +0530

    Time: 4 ms (98.07%), Space: 45.8 MB (49.06%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static void dfs(int[][] matrix, boolean[][] ocean, int r, int c, int curr) {
+        if (r < 0 || r >= matrix.length || c < 0 || c >= matrix[0].length) return;
+        if (matrix[r][c] < curr || ocean[r][c] == true) return;
+        ocean[r][c] = true;
+        // down, up, right, left
+        dfs(matrix, ocean, r + 1, c, matrix[r][c]);
+        dfs(matrix, ocean, r - 1, c, matrix[r][c]);
+        dfs(matrix, ocean, r, c + 1, matrix[r][c]);
+        dfs(matrix, ocean, r, c - 1, matrix[r][c]);
+    }
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> list = new ArrayList<>();
+        int row = heights.length, col = heights[0].length;
+        boolean[][] pacific = new boolean[row][col];
+        boolean[][] atlantic = new boolean[row][col];
+        int MIN = Integer.MIN_VALUE;
+        // first row & last row
+        for (int c = 0; c < col; c++) {
+            dfs(heights, pacific, 0, c, MIN);
+            dfs(heights, atlantic, row - 1, c, MIN);
+        }
+        // first col & last col
+        for (int r = 0; r < row; r++) {
+            dfs(heights, pacific, r, 0, MIN);
+            dfs(heights, atlantic, r, col - 1, MIN);
+        }
+        for (int r = 0; r < row; r++) {
+            for (int c = 0; c < col; c++) {
+                if (pacific[r][c] == true && atlantic[r][c] == true) {
+                    list.add(Arrays.asList(r, c));
+                }
+            }
+        }
+        return list;
+    }
+}
