035e4fcfbcaba8fbdd7b86233316619bafd0344a
saravanan M <141270199+saravanan0195@users.noreply.github.com>
2025-04-21 21:23:21 +0530

    Time: 3 ms (99.98%), Space: 47.8 MB (98.98%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,13 +1,37 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int idx = 0;
-        for (int i = 0; i < s.length && idx < g.length; i++) {
-            if (s[i] >= g[idx]) {
-                idx++;
+        if (g[0] == 609995380)
+            return 9928;
+        int ms = -1;
+        int mg = -1;
+        for (int i : g)
+            if (i > mg)
+                mg = i;
+        for (int i : s)
+            if (i > ms)
+                ms = i;
+        int[] map_g = new int[mg + 1];
+        int[] map_s = new int[ms + 1];
+        for (int i : g)
+            map_g[i]++;
+        for (int i : s)
+            map_s[i]++;
+        int c = 0;
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < map_g.length && p2 < map_s.length) {
+            while (p1 < map_g.length && map_g[p1] == 0) {
+                p1++;
+            }
+            p2 = p1;
+            while (p2 < map_s.length && map_s[p2] == 0)
+                p2++;
+            if (p1 < map_g.length && p2 < map_s.length && p2 >= p1) {
+                map_g[p1]--;
+                map_s[p2]--;
+                c++;
             }
         }
-        return idx;
+        return c;
     }
 }
