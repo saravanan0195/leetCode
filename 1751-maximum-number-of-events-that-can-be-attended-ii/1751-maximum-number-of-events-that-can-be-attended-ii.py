c8e599dc8387fc485f90f09826a72721133ec096
saravanan M <141270199+saravanan0195@users.noreply.github.com>
2025-07-08 23:37:06 +0530

    Time: 381 ms (98.35%), Space: 71.9 MB (78.57%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,15 +1,24 @@
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
-        @cache
-        def dfs(i: int, k: int) -> int:
-            if i >= len(events):
-                return 0
-            _, ed, val = events[i]
-            ans = dfs(i + 1, k)
-            if k:
-                j = bisect_right(events, ed, lo=i + 1, key=lambda x: x[0])
-                ans = max(ans, dfs(j, k - 1) + val)
-            return ans
+        events,n=sorted(events, key=lambda e:e[1]), len(events)
+        events_start_sorted = sorted([(e[0], i) for i,e in enumerate(events)])
+        preceding,j = [-1]*n,0
+        for start, index in events_start_sorted:            
+            while events[j][1]<start:
+                j+=1
+            preceding[index]=j-1
 
-        events.sort()
-        return dfs(0, k)  
+        dp,res = [0]*n,0
+        for j in range(1, k+1):
+            max_value=-1
+            dp_next=[-1]*n            
+            for i in range(n):
+                if j==1:
+                    max_value=max(max_value, events[i][2])                    
+                elif preceding[i]>=0 and dp[preceding[i]]>=0:
+                    max_value=max(max_value, dp[preceding[i]]+events[i][2])
+                dp_next[i]=max_value                
+            dp=dp_next
+            res=max(res, max_value)
+        
+        return res
