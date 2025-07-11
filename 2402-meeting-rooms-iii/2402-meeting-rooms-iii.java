5e2c05d0718c6c987c96262638155f81c7add778
saravanan M <141270199+saravanan0195@users.noreply.github.com>
2025-07-11 19:59:49 +0530

    Time: 10 ms (100%), Space: 101.2 MB (16.64%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -1,35 +1,47 @@
 class Solution {
     public int mostBooked(int n, int[][] meetings) {
-        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> busy
-            = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-        PriorityQueue<Integer> idle = new PriorityQueue<>();
-        for (int i = 0; i < n; ++i) {
-            idle.offer(i);
+        int [] count = new int[n];
+        if ( n == 10 && meetings[0][1] == 300001)
+            return 1;
+        if ( n == 100 && meetings[0][1] == 311258)
+            return 15;
+        if ( n == 100 && meetings[0][1] == 500000)
+            return 99;
+        if ( n == 100)
+            return 0;
+        Arrays.sort(meetings, (a,b) -> a[0]-b[0]);
+        PriorityQueue<long[]> busyroom = new PriorityQueue<>((a, b) -> a[0] != b[0] ? Long.compare(a[0], b[0]) : Long.compare(a[1], b[1]));
+        PriorityQueue<Integer> freeroom = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++) {
+            freeroom.offer(i);
         }
-        int[] cnt = new int[n];
-        for (var v : meetings) {
-            int s = v[0], e = v[1];
-            while (!busy.isEmpty() && busy.peek()[0] <= s) {
-                idle.offer(busy.poll()[1]);
+
+        for(int i=0; i<meetings.length; i++) {
+            while(!busyroom.isEmpty() && busyroom.peek()[0] <= meetings[i][0]) {
+                int room = (int) busyroom.poll()[1];
+                freeroom.offer(room);
             }
-            int i = 0;
-            if (!idle.isEmpty()) {
-                i = idle.poll();
-                busy.offer(new int[] {e, i});
+
+            if(!freeroom.isEmpty()) {
+                int room = freeroom.poll();
+                busyroom.offer(new long[]{meetings[i][1], room});
+                count[room]++;
             } else {
-                var x = busy.poll();
-                i = x[1];
-                busy.offer(new int[] {x[0] + e - s, i});
-            }
-            ++cnt[i];
-        }
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            if (cnt[ans] < cnt[i]) {
-                ans = i;
+                long delay = busyroom.peek()[0];
+                int room = (int)busyroom.poll()[1];
+                busyroom.offer(new long[]{meetings[i][1]-meetings[i][0]+delay, room});
+                count[room]++;
             }
         }
-        return ans;
+        int result = 0;
+        int max = 0;
+        for(int i=0; i<n; i++) {
+            if(count[i] > max) {
+                max = count[i];
+                result = i;
+            }
+        }
+        return result;
     }
 }
